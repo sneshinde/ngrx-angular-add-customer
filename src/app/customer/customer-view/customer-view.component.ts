2384e9d9ac512e47aa0709037895169f4541b0d4
Snehal Shinde <snehalshinde@Snehals-MacBook-Air.local>
2021-03-29 13:19:40 +0530

    added code to add customer

---
+++ src/app/customer/customer-view/customer-view.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { select } from '@ngrx/store';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Customer } from 'src/app/models/customer';
+import { CustomerState } from '../store/reducer/customer.reducer';
+import { selectCustomers } from '../store/selector/customer.selectors';
+
+@Component({
+  selector: 'app-customer-view',
+  templateUrl: './customer-view.component.html',
+  styleUrls: ['./customer-view.component.scss']
+})
+export class CustomerViewComponent implements OnInit {
+  customers$: Observable<Customer[]>;
+  
+  constructor(private store: Store<CustomerState>) { 
+    this.customers$ = this.store.pipe(select(selectCustomers));
+  }
+
+  ngOnInit() {
+    
+  }
+
+}
---
+++ src/app/customer/store/action/customer.actions.ts
@@ -0,0 +1,20 @@
+import { createAction, props } from '@ngrx/store';
+import { Customer } from 'src/app/models/customer';
+
+export const addCustomer = createAction(
+  'Customer Add Customer',
+  (customer: Customer) => ({customer})
+);
+export const loadCustomers = createAction(
+  '[Customer] Load Customers'
+);
+
+export const loadCustomersSuccess = createAction(
+  '[Customer] Load Customers Success',
+  props<{ data: any }>()
+);
+
+export const loadCustomersFailure = createAction(
+  '[Customer] Load Customers Failure',
+  props<{ error: any }>()
+);
